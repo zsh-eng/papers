3474554f67ebebf6d68efd6b21bd26487d330b07
zsheng <zsheng.dev@gmail.com>
2026-01-11 18:53:51 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { ComponentExample } from "@/components/component-example";
+
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+return <ComponentExample />;
 }
 
-export default App
+export default App;
